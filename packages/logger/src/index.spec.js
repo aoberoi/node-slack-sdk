b7ef1c1d3ba8b046e5b4e4edbd175990853aec27
Ankur Oberoi <aoberoi@gmail.com>
2019-10-08 16:44:52 -0400

    reverts adding getLevel to the Logger interface to preserve backwards compatibility in @slack/logger@v1

--- packages/logger/src/index.spec.js
+++ packages/logger/src/index.spec.js
@@ -2,41 +2,21 @@ require('mocha');
 const { assert } = require('chai');
 const { ConsoleLogger, LogLevel } = require('./index');
 
+// NOTE: this test file uses the private property `.level` for testing purposes.
+
 describe('logger', () => {
   it('should have the default LogLevel', () => {
     const logger = new ConsoleLogger();
-    assert.equal(logger.getLevel(), LogLevel.INFO);
+    assert.equal(logger.level, LogLevel.INFO);
   });
 
-  it('should set LogLevel corrrectly', () => {
+  it('should set LogLevel correctly', () => {
     const logger = new ConsoleLogger();
-    assert.equal(logger.getLevel(), LogLevel.INFO);
+    assert.equal(logger.level, LogLevel.INFO);
 
     [LogLevel.DEBUG, LogLevel.ERROR, LogLevel.WARN, LogLevel.INFO].forEach((level) => {
       logger.setLevel(level);
-      assert.equal(logger.getLevel(), level);
+      assert.equal(logger.level, level);
     });
-  });
-
-  it('should offer getLevel to test which level is currently set', () => {
-    let largeObjectGenerated = false;
-    function generateSomethingExpensive() {
-      largeObjectGenerated = true;
-      return JSON.stringify('{ description: "Something expensive to load" }');
-    }
-
-    const logger = new ConsoleLogger();
-    logger.setLevel(LogLevel.INFO);
-    if (logger.getLevel() === LogLevel.DEBUG) {
-      const largeObj = generateSomethingExpensive();
-      logger.debug(`debug: ${largeObj}`);
-    }
-    assert.isFalse(largeObjectGenerated);
-
-    logger.setLevel(LogLevel.DEBUG);
-    if (logger.getLevel() === LogLevel.DEBUG) {
-      generateSomethingExpensive();
-    }
-    assert.isTrue(largeObjectGenerated);
   });
 });
